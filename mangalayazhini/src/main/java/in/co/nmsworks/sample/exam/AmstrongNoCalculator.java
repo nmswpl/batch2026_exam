920fbb21a6b2bbed35ad98487d72026f1639c61e
Mangala Yazhini <mangalayazhini@nmsworks.co.in>
2026-07-07 10:45:17 +0530

    AmstrongNoCalculator program completed

---
+++ mangalayazhini/src/main/java/in/co/nmsworks/sample/exam/AmstrongNoCalculator.java
@@ -0,0 +1,36 @@
+package in.co.nmsworks.sample.exam;
+
+public class AmstrongNoCalculator {
+
+    public static void main(String[] args) {
+        AmstrongNoCalculator calculatorObj = new AmstrongNoCalculator();
+
+        calculatorObj.checkAmstrongNo(123);
+    }
+
+    private void checkAmstrongNo(int number) {
+        int originalNumber = number;
+        String numberStr = String.valueOf(number);
+        int noOfDigits = 0 ;
+        while(number > 0 ){
+            int quotient = number/10;
+            number = quotient ;
+            noOfDigits++;
+        }
+
+        int sum = 0 ;
+        for (int i = 0; i < numberStr.length(); i++) {
+            int value = numberStr.charAt(i) - 48;
+            sum += Math.pow((double)value, (double)noOfDigits);
+        }
+
+        if(sum != originalNumber){
+            System.out.println(originalNumber+" is not an amstrong no");
+        }
+        else{
+            System.out.println(originalNumber+" is an amstrong no");
+        }
+//        System.out.println("sum : " + sum);
+//        System.out.println("No of digits : " + noOfDigits);
+    }
+}

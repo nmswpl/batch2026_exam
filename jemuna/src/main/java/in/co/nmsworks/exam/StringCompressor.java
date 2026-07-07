fd48f36ca74e0110af57732b553e900b69f899e4
Jemuna <jemunau@nmsworks.co.in>
2026-07-07 13:02:55 +0530

    string

---
+++ jemuna/src/main/java/in/co/nmsworks/exam/StringCompressor.java
@@ -0,0 +1,16 @@
+package in.co.nmsworks.exam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringCompressor {
+    public static void main(String[] args) {
+        compressString("aaabbca");
+    }
+
+    private static void compressString(String input) {
+        int sizeOfString = input.length();
+
+
+    }
+}

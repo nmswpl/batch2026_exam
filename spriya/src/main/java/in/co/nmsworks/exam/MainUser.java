bc3d51d98b75ea1365c8e094af877d2a8df99770
PriyadharshiniS <spriyadharshini@nmsworks.co.in>
2026-07-07 12:25:27 +0530

    Valid username and password question

--- spriya/src/main/java/in/co/nmsworks/exam/MainUser.java
+++ spriya/src/main/java/in/co/nmsworks/exam/MainUser.java
@@ -4,17 +4,17 @@ public class MainUser {
     public static void main(String[] args) {
         UserPassword userPassword=new UserPassword();
 
-//        userPassword.readDb();
-//        System.out.println("Test case for valid");
-//       userPassword.check("felsya","bS2`N4CtBB");
-//        System.out.println();
-//        System.out.println("Test case for invalid password");
-//        userPassword.check("felsya","bS2`N4CtB");
-//        System.out.println();
-//        System.out.println("Test case for Invalid uername");
-//        userPassword.check("felya","bS2`N4CtBB");
 
-        userPassword.readDbForActiveList();
+        userPassword.readDb();
+        System.out.println("Test case for valid");
+       userPassword.check("felsya","bS2`N4CtBB");
+        System.out.println();
+        System.out.println("Test case for invalid password");
+        userPassword.check("felsya","bS2`N4CtB");
+        System.out.println();
+        System.out.println("Test case for Invalid uername");
+        userPassword.check("felya","bS2`N4CtBB");
+
 
 
 
---
+++ spriya/src/main/java/in/co/nmsworks/exam/UserPassword.java
@@ -0,0 +1,62 @@
+package in.co.nmsworks.exam;
+
+
+import java.sql.*;
+import java.util.*;
+
+public class UserPassword {
+
+   List< List<String> >list=new ArrayList<>();
+    public void readDb(){
+
+        try(Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/exam2026");
+            PreparedStatement statement=con.prepareStatement("select username,password from user_details");
+            Scanner sc=new Scanner(System.in);){
+
+            ResultSet rs= statement.executeQuery();
+            while(rs.next()){
+
+
+                String userName=rs.getString(1);
+                String password=rs.getString(2);
+                List<String> smallList=new ArrayList<>();
+                smallList.add(userName);
+                smallList.add(password);
+
+                list.add(smallList);
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void check(String name,String secret){
+
+
+
+        for (int i = 0; i < list.size(); i++) {
+            String nameValue=list.get(i).get(0);
+            String password=list.get(i).get(1);
+            if(name.equals(nameValue)){
+
+                if(secret.equals(password)){
+                    System.out.println("Password Valid");
+                    return;
+                }
+                else{
+                    System.out.println("Invalid password");
+                    return;
+                }
+            }
+
+        }
+
+            System.out.println("Invalid Username");
+
+        }
+    }
+

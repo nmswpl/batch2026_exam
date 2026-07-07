bb18bfbc8543ceab71eb96e87671963e77094401
Jemuna <jemunau@nmsworks.co.in>
2026-07-07 11:37:03 +0530

    Sales csv

---
+++ jemuna/src/main/java/in/co/nmsworks/exam/SalesEntry.java
@@ -0,0 +1,71 @@
+package in.co.nmsworks.exam;
+
+
+public class SalesEntry {
+    String sales_id;
+    String product_name;
+    String category;
+    int price;
+    int items_sold;
+    String date;
+
+    public SalesEntry(String sales_id, String product_name, String category, int price, int items_sold, String date) {
+        this.sales_id = sales_id;
+        this.product_name = product_name;
+        this.category = category;
+        this.price = price;
+        this.items_sold = items_sold;
+        this.date = date;
+    }
+
+    public SalesEntry() {
+    }
+
+    public String getSales_id() {
+        return sales_id;
+    }
+
+    public void setSales_id(String sales_id) {
+        this.sales_id = sales_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getItems_sold() {
+        return items_sold;
+    }
+
+    public void setItems_sold(int items_sold) {
+        this.items_sold = items_sold;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
---
+++ jemuna/src/main/java/in/co/nmsworks/exam/SalesInfo.java
@@ -0,0 +1,62 @@
+package in.co.nmsworks.exam;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static jdk.nashorn.internal.objects.NativeMath.max;
+
+public class SalesInfo {
+    public static void main(String[] args) {
+        SalesInfo salesinfo = new SalesInfo();
+        Set<String> categorySet = new HashSet<>();
+        salesinfo.averagePrice(categorySet);
+        SalesEntry sales = new SalesEntry();
+    }
+
+    private void averagePrice(Set<String> categorySet) {
+
+        try(BufferedReader reader = new BufferedReader(new FileReader("/home/nms-training/Downloads/Exam_2026/SALES_DATA.csv"))){
+            String line;
+            int averagePrice = 0;
+            int sum = 0;
+            int wordlength;
+            while((line = reader.readLine())!=null){
+                reader.readLine();
+                String[] words = line.split(",");
+                for (String word : words) {
+                    System.out.println(word);
+                }
+                wordlength = words.length;
+                for (int i = 0; i < words.length; i++) {
+                    String sales_id = words[0];
+                    String product_name = words[1];
+                    String category = words[2];
+                    categorySet.add(words[2]);
+                    int price = Integer.parseInt(words[3]);
+                    int items_sold = Integer.parseInt(words[4]);
+                    String date = words[5];
+                    SalesEntry sales = new SalesEntry(sales_id,product_name,category,price,items_sold,date);
+                    for (String eachCategory : categorySet) {
+                        if(sales.category.equals(eachCategory)){
+                            sum+=sales.getPrice();
+                            averagePrice = sum/wordlength;
+                            System.out.println("Average of Price by Category :"+averagePrice);
+                        }
+                    }
+                    int maxSoldItems = (int) max(sales.getItems_sold());
+                    System.out.println("Max Sold item :"+maxSoldItems);
+                }
+
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+}

5f0c0990295d26c72972e0adcd47b0e032071edd
Gabriella D <dgabriella@nmsworks.co.in>
2026-07-07 11:56:04 +0530

    Armstrong number modified

--- gabriella/src/main/java/in/co/nmsworks/exam/ArmstrongNumber.java
+++ gabriella/src/main/java/in/co/nmsworks/exam/ArmstrongNumber.java
@@ -3,28 +3,24 @@ package in.co.nmsworks.exam;
 public class ArmstrongNumber {
     public static void main(String[] args) {
         ArmstrongNumber armstrongNumber = new ArmstrongNumber();
-        for (int i = 10; i < 1000; i++){
-            if (armstrongNumber.checkArmstrongNumber(i)) {
+        for (int i = 10; i < 10000; i++){
+            if (armstrongNumber.isArmstrongNumber(i)) {
                 System.out.println(i);
             }
         }
     }
 
-    private boolean checkArmstrongNumber(int num) {
+    private boolean isArmstrongNumber(int num) {
         int sumOfDigits = 0;
         String number = String.valueOf(num);
         int numLength = number.length();
         int tmp = num;
 
         while (tmp != 0){
-            tmp = tmp / 10;
             int rem = tmp % 10;
-            int value = 1;
-            for (int i = 0; i < numLength; i++){
-                value *= rem;
-            }
+            tmp = tmp / 10;
+            int value = (int) Math.pow(rem, numLength);
             sumOfDigits += value;
-
         }
         if (num == sumOfDigits) return true;
         else return false;

a4b37f809a5a3a28cdc7678704bc794918133924
Manikandan Sakthi S <manikandansakthi@nmsworks.co.in>
2026-07-07 10:43:59 +0530

    2.StringCompressor

---
+++ sakthi/src/main/java/in/co/nmsworks/Exam/StringCompressor.java
@@ -0,0 +1,38 @@
+package in.co.nmsworks.Exam;
+
+public class StringCompressor {
+
+    private static String compressString(String input) {
+
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+
+        StringBuilder compressed = new StringBuilder();
+        int count = 1;
+
+
+        for (int i = 0; i < input.length(); i++) {
+
+            if (i + 1 < input.length() && input.charAt(i) == input.charAt(i + 1)) {
+                count++;
+            } else {
+
+                compressed.append(count).append(input.charAt(i));
+                count = 1;
+            }
+        }
+
+        return compressed.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String input = "aaabbca";
+        String output = compressString(input);
+
+        System.out.println("Input:  \"" + input + "\"");
+        System.out.println("Output: \"" + output + "\"");
+    }
+}
+
